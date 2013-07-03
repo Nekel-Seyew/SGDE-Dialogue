c48c589bc9440b2faefe01891354aeee0de5537a
kdsweenx <kdsweenx@KDSWEENX-MOBL1.amr.corp.intel.com>
2013-07-03 10:08:28 -0700

    Starting to begin major work. This should be done today....

---
+++ SGDE-Dialogue/src/sgde/dialogue/DialogueOption.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sgde.dialogue;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kdsweenx
+ */
+public class DialogueOption {
+    String text;
+    int place;
+    ArrayList<DialogueOption> nodes;
+    
+    public DialogueOption(int place){
+        
+    }
+    
+  
+    
+}
---
+++ SGDE-Dialogue/src/sgde/dialogue/TestPerson.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sgde.dialogue;
+
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author kdsweenx
+ */
+public class TestPerson implements DialoguePerson{
+
+    @Override
+    public void create(String filePath) throws FileNotFoundException {
+        
+    }
+
+    @Override
+    public void start() {
+        
+    }
+    
+}
